4c8371d51a9c1259fdf614b79e56c91e695e5f4b
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-01-13 17:14:31 -0300

    se creó el proyecto, instaló bootstrap, se limpió el archivo y se realizó el primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center my-3'>Ejercicio Seis: Paleta de Colores</h1>
+    </>
+  )
+}
+
+export default App
